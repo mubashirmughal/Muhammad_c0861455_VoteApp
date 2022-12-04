12310e1a39eb6105defc052f9bc7a8effed34a49
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-12-03 21:32:41 -0500

    added layout and strings

--- app/src/main/java/com/example/votingapp/MainActivity.java
+++ app/src/main/java/com/example/votingapp/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.votingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    Spinner droupdown;
+    EditText name , Vote_Id;
+    ToggleButton tog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
